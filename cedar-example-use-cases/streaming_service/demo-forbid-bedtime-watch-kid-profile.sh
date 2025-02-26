9dba84e2d309bc4587a9378628a564c70feb3b5d
Adrian Palacios <accorell@amazon.com>
2025-02-26 14:46:30 -0500

    Prepare demo for streaming service
    Signed-off-by: Adrian Palacios <accorell@amazon.com>

---
+++ cedar-example-use-cases/streaming_service/demo-forbid-bedtime-watch-kid-profile.sh
@@ -0,0 +1,4 @@
+cedar authorize --policies policies.cedar \
+                --schema policies.cedarschema \
+                --schema-format cedar \
+                --entities entities.json --request-json DENY/dave_watch_bedtime_show.json
